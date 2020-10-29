19e17bb5cdfd0dc788405766867a1e75243d41d3
Charles Forsyth <charles.forsyth@ucr.edu>
2020-10-29 13:20:37 -0700

    good

---
+++ int.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+namedefault="test"
+imagedefault="centos:latest"
+cmddefault="bash"
+
+name=${1:-$namedefault}
+image=${3:-$imagedefault}
+cmd=${2:-$cmddefault}
+
+if kubectl get pods --no-headers --selector=job-name=$name 2>/dev/null | grep $name
+  then
+    echo "Job with that name found"
+    kubectl delete job $name
+    name="tempjob"
+  else
+    true
+fi
+
+cat <<EOF | kubectl apply -f -
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: $name
+spec:
+  template:
+    spec:
+      containers:
+      - name: blast
+        image: $image
+        command: ["sleep", "infinity"]
+        volumeMounts:
+        - mountPath: /sharedvol
+          name: sharedvol
+        resources:
+          limits:
+            memory: 4Gi
+            cpu: "4"
+          requests:
+            memory: 1Gi
+            cpu: "1"
+      volumes:
+      - name: sharedvol
+        persistentVolumeClaim:
+          claimName: master-vol
+      - name: git-repo
+        emptyDir: {}
+      restartPolicy: Never
+  backoffLimit: 1
+EOF
+
+podname=$(kubectl get pods --no-headers --selector=job-name=$name | awk '{print $1}')
+echo $podname
+
+until kubectl get pods --no-headers $podname | grep "Running"
+do
+  kubectl get pods --no-headers $podname
+  sleep 1
+done
+kubectl exec -it $podname $cmd
+kubectl delete job $name
